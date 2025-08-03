fe02cc3ebbaeb1b404f4b646ba721438c852feed
Shiva Sai <57152818+Shiva-7@users.noreply.github.com>
2025-08-03 07:17:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,7 @@ we are creating this terraform file to push this into github from the git
 without pulling the changes from git at first place let see 
 ####################
 adding this data from main breanch to see if coiflicts occur
+
+#################
+
+Adding this from github
