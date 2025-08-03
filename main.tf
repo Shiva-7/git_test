c49d217d83f5d5c9eff2de54c071589e65f4fe2d
root <root@ip-172-31-84-117.ec2.internal>
2025-08-03 01:41:41 +0000

    adding main.tf to check conflicts

--- main.tf
+++ main.tf
@@ -1,2 +1,4 @@
 we are creating this terraform file to push this into github from the git 
 without pulling the changes from git at first place let see 
+####################
+adding this data from main breanch to see if coiflicts occur
